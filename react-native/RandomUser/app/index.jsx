8942a937b9e9c7d297e105ddd8c440c767ebda64
Ian Chen <chenian317@gmail.com>
2025-04-07 22:11:42 -0300

    terminar ejercicio de usuarios random

--- react-native/RandomUser/app/index.jsx
+++ react-native/RandomUser/app/index.jsx
@@ -22,69 +22,83 @@ const UserInput = () => {
     );
 }
 */}
+
+
 const UserResults = props => {
+  const myUsers = props[1];
   return (
     <View>
-    <Text>{props.isVisible ? props.users :null}</Text>
+      <Text>{myUsers}</Text>
     </View>
   );
 }
 
 export default function Index() {
+
   const [numberOfUsers, setNumber] = useState("");
-  const apiUrl = "https://randomuser.me/api/";
-  let allUsers = [];
-  let resultVisible=false;
-  
+  const [resultVisible, setResultVisible] = useState(false);
+  const [allUsers, setAllUsers] = useState([]);
+
+
   {/* Sleep function */}
   const sleep = (ms) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
   };
 
-  {/* Fetch API */}
-  async function fetchUsers (){
+
+  /* Fetch API */
+  async function fetchUsers(){
     try {
-      {/* Fetch la cantidad de usuarios que pidieron*/}
-      for (let i=1; i<=numberOfUsers; i++){
-        const response = await Axios.get(apiUrl);
-        const user = response.data.results[0];
-        const userName = `${user.name.first} ${user.name.last}`;
+      const apiUrl = "https://randomuser.me/api/?results=" + numberOfUsers;
+      const response = await Axios.get(apiUrl);
+      let users = [];
 
-        /* ALmacenar data*/
-        allUsers.push(
-          <View>
-            <Text>{userName}</Text>
-          </View>
-          );
-        await sleep(200);
+      {/* Limpiar data */}
+      for (let i=0; i<numberOfUsers; i++){
+        const userData = response.data.results[i];
+        const userName = `${userData.name.first} ${userData.name.last}`;
+        users.push(
+
+          <Text> {userName} {"\n"}</Text>
+        );
       }    
+      
+      /* ALmacenar data */
+      setAllUsers(users);
 
-      resultVisible=true;
-      console.log(resultVisible)
       console.log(allUsers);
-
+    
     } catch(error) {
       console.error(error);
     }
   }
 
-
   return (
-    <View style={{padding: 10}}>
+    <View style={{
+      flex: 1,
+      padding: 10, 
+      justifyContent: "center", 
+      alignContent: "center"}}>
+      
       <TextInput
-      style={{height: 40, padding: 5, borderBlockColor: "#00000"}}
       placeholder="Ingrese la cantidad de usuarios a buscar"
       onChangeText={newNumber => setNumber(newNumber)}
       defaultValue={numberOfUsers}
       /> 
 
-    <Button 
-      title="Send"
-      onPress={fetchUsers}
-    />
+      <Button 
+        title="Send"
+        onPress={async () => {
+          setResultVisible(true);
+          await fetchUsers();
+        }}
+      />
 
-    <UserResults isVisible={resultVisible} users={allUsers}/>
-    <Text> {resultVisible} </Text>
+    {/* Imprimir resultado de búsqueda */}
+    {resultVisible && <ScrollView>
+      <Text>{allUsers}</Text>      
+    
+    </ScrollView>}
     </View>
   );
 }
